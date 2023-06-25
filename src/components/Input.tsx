67a2ea3c2d14c873275117218e03965b126ec726
bilal.zahory <bilal.zahory@consensys.net>
2023-06-25 19:51:21 -0400

    style: prettier

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,16 @@
 import type { InputHTMLAttributes } from "react";
 
-
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    className?: string
-    handleChange?: (newText: string) => void
-
+  className?: string;
+  handleChange?: (newText: string) => void;
 }
 
 export default function Input({ className, handleChange, ...rest }: Props) {
-    return <input className={`w-3/4 self-center rounded-md p-4 ${className}`} onChange={(event) => handleChange && handleChange(event.target.value)} {...rest} />
+  return (
+    <input
+      className={`w-3/4 self-center rounded-md p-4 ${className}`}
+      onChange={(event) => handleChange && handleChange(event.target.value)}
+      {...rest}
+    />
+  );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,56 +1,69 @@
-import React, { useState } from 'react'
-import Input from '../../components/Input';
+import React, { useState } from "react";
+import Input from "../../components/Input";
 
-interface Props { }
+interface Props {}
 
 // handles both login and sign-up
-export default function Login({ }: Props) {
-    const [isSigningUp, setIsSigningUp] = useState(false)
+export default function Login({}: Props) {
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+  const handleSubmit = () => {
+    // TODO: reference API here
+  };
 
-    const handleSubmit = () => {
-        // TODO: reference API here
-    }
+  return (
+    <main className="flex flex-col min-h-screen gap-y-10 justify-center items-center">
+      <img src="/logo.png" alt="logo" className="w-16 sm:w-20" />
 
-    return (<main
-        className='flex flex-col min-h-screen gap-y-10 justify-center items-center'
-    >
-        <img src='/logo.png' alt="logo" className="w-16 sm:w-20" />
-
-        <div className={`flex flex-col w-3/4 sm:w-1/2 bg-white rounded-xl overflow-hidden divide-y border [&>*]:p-4`}>
-            <h1 className="text-2xl w-full text-center">Welcome{!isSigningUp && ' back'}!</h1>
-            <div className="bg-gray-100">
-                <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
-                    {isSigningUp && <Input
-                        required={isSigningUp}
-                        type="email"
-                        placeholder="Email"
-                        handleChange={setEmail}
-                        value={email}
-                    />}
-                    <Input
-                        required
-                        placeholder="Username"
-                        type="text"
-                        value={userName}
-                        handleChange={setUserName}
-                    />
-                    <Input
-                        required
-                        placeholder="Password"
-                        type="password"
-                        value={password}
-                        handleChange={setPassword}
-                    />
-                    <Input type="submit" value={isSigningUp ? 'Sign Up' : 'Login'} className="bg-blue-500 hover:bg-opacity-90 text-white rounded-full w-1/2 self-center p-2 cursor-pointer" />
-                </form>
-            </div>
-            <button className="text-blue-500 underline p-4" onClick={() => setIsSigningUp(!isSigningUp)}>{isSigningUp ? "Already" : "Don't"} have an account?</button>
+      <div
+        className={`flex flex-col w-3/4 sm:w-1/2 bg-white rounded-xl overflow-hidden divide-y border [&>*]:p-4`}
+      >
+        <h1 className="text-2xl w-full text-center">
+          Welcome{!isSigningUp && " back"}!
+        </h1>
+        <div className="bg-gray-100">
+          <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+            {isSigningUp && (
+              <Input
+                required={isSigningUp}
+                type="email"
+                placeholder="Email"
+                handleChange={setEmail}
+                value={email}
+              />
+            )}
+            <Input
+              required
+              placeholder="Username"
+              type="text"
+              value={userName}
+              handleChange={setUserName}
+            />
+            <Input
+              required
+              placeholder="Password"
+              type="password"
+              value={password}
+              handleChange={setPassword}
+            />
+            <Input
+              type="submit"
+              value={isSigningUp ? "Sign Up" : "Login"}
+              className="bg-blue-500 hover:bg-opacity-90 text-white rounded-full w-1/2 self-center p-2 cursor-pointer"
+            />
+          </form>
         </div>
-
-    </main>)
+        <button
+          className="text-blue-500 underline p-4"
+          onClick={() => setIsSigningUp(!isSigningUp)}
+        >
+          {isSigningUp ? "Already" : "Don't"} have an account?
+        </button>
+      </div>
+    </main>
+  );
 }
