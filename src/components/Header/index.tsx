0713376bd56f2f2b9ba4bf4753464bca251f5852
bilal.zahory <bilal.zahory@consensys.net>
2023-06-25 20:38:01 -0400

    chore(site): use Link instead of a tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Tab } from "@headlessui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const ITEM_CLASSES = (isSelected: boolean) =>
@@ -28,23 +28,23 @@ export default function Header({}: Props) {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <img src="/logo.png" alt="logo" className="w-8 mr-3" />
 
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Tad-Thrust
           </span>
-        </a>
+        </Link>
         <div className="block w-auto" id="navbar-default">
           <ul className="font-medium flex border-gray-100 rounded-lg flex-row space-x-8 mt-0 border-0">
             {NAV_ITEMS.map(({ path: routePath, name }) => (
               <li>
-                <a
+                <Link
                   href={routePath}
                   className={ITEM_CLASSES(currentPath === routePath)}
                 >
                   {name}
-                </a>
+                </Link>
               </li>
             ))}
           </ul>
