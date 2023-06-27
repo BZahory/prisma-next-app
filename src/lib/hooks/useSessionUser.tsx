3e145318b062d5c50222664f18955f0b3def7b93
bilal.zahory <bilal.zahory@consensys.net>
2023-06-26 21:30:33 -0400

    chore(site): add predicate

--- src/lib/hooks/useSessionUser.tsx
+++ src/lib/hooks/useSessionUser.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { User } from "../types/sql";
 import { createAuthHeader } from "../utils/auth";
@@ -11,12 +12,14 @@ export default function useDatabaseUser() {
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/user/${sessionUser?.username}`, {
-      method: "GET",
-      headers,
-    }).then(async (res) => {
-      setUser(await res.json());
-    });
+    if (sessionUser) {
+      fetch(`http://localhost:3000/api/user/${sessionUser.username}`, {
+        method: "GET",
+        headers,
+      }).then(async (res) => {
+        setUser(await res.json());
+      });
+    }
   }, [sessionUser]);
 
   return user;
